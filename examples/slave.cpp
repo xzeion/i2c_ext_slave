24e0a14201ea37191b56d629c709b499214dd477
xzeion <xzeion@gmail.com>
2014-10-02 18:01:31 -0400

    Started adding registers

--- examples/slave.cpp
+++ examples/slave.cpp
@@ -2,7 +2,7 @@
 // Wire Slave Sender
 // by Nicholas Zambetti <http://www.zambetti.com>
 
-// Demonstrates use of the Wire library
+// Demonstrates use of the Wire libraryk
 // Sends data as an I2C/TWI slave device
 // Refer to the "Wire Master Reader" example for use with this
 
@@ -22,19 +22,15 @@ void setup()
       pinMode(13,OUTPUT);
 }
 
+int count;
 void loop()
 {
-      digitalWrite(13,HIGH);
-      delay(100);
-      digitalWrite(13,LOW);
-      delay(100);
-      Serial.println("Slave is working");       
+     // digitalWrite(13,HIGH);
+     // delay(100);
+     // digitalWrite(13,LOW);
+     // delay(100);
+     // Serial.print("Slave is working: ");       
+     // count++;
+     // Serial.println(count);
 }
 
-// function that executes whenever data is requested by master
-// this function is registered as an event, see setup()
-//void requestEvent()
-//{
-//      Wire.write("hello "); // respond with message of 6 bytes
-                             // as expected by master
-//}
--- wireextender_slave.cpp
+++ wireextender_slave.cpp
@@ -9,7 +9,7 @@ By Brian Gratton and James Bliss 07/27/14*/
 #include <wireextender_slave.h>
 
 int i2c_addr = 2;
-int reg = 1;
+int type_select = 0;
 
 void requestEvent(void);
 void receiveEvent(int);
@@ -46,29 +46,31 @@ typedef union {
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-void requestEvent(){ // write selected buffer to wire
-    
-    switch(reg){
+void requestEvent(){
+    // Writes the selected message buffer to the wire
+    // Recieve event always happens first to select the register
+    switch(type_select){
     case 1:
-        Wire.write((const char*)message_8,1);
+        message_8 = 5;
+        Wire.write(&message_8,sizeof(uint8_t));
     break;
 
     case 2:
         message_16 buffer_16;
-        buffer_16.int16 = 161616;
-        Wire.write((const char*)buffer_16.bytes,2);
+        buffer_16.int16 = 16161;
+        Wire.write((const char*)buffer_16.bytes,sizeof(uint16_t));
     break;
 
     case 3:
         message_32 buffer_32;
-        buffer_16.int16 = 32323232;
-        Wire.write((const char*)buffer_16.bytes,4);
+        buffer_32.int32 = 32323232;
+        Wire.write((const char*)buffer_32.bytes,sizeof(uint32_t));
     break;
 
     case 4:
         message_float buffer_f;
         buffer_f.floatingPoint = 3.14;
-        Wire.write((const char*)buffer_f.bytes,5);
+        Wire.write((const char*)buffer_f.bytes,sizeof(double));
     break;
     }
 }
@@ -78,32 +80,30 @@ void requestEvent(){ // write selected buffer to wire
 typedef union {
     uint8_t bytes[sizeof(uint16_t)];
     uint16_t int16;
-} regbuff;
+} type_buff;
+
+typedef union {
+  uint8_t bytes[sizeof(uint32_t)];
+  uint32_t int32;
+} write_buff;
 
 void receiveEvent(int numBytes){ // take data in from wire
-   // regbuff reg_num;
-   // message buffer;
+    type_buff ts;
 
-   /* for(int i; i>2; i++){
-        reg_num.bytes[i] = Wire.read();
+    for(int i; i<sizeof(uint16_t); i++){
+      ts.bytes[i] = Wire.read();
+    }
+    Serial.println(ts.int16);
+    type_select = ts.int16;
+    //call function to set the data to register then remove previous line
+
+    write_buff wb;
+    for(int r; r<Wire.available(); r++){
+       wb.bytes[r] = Wire.read();
     }
     
-    */
-//TODO:If the wire is still Avialable then read in the message and write it to the register recieved
-    char bytes[6];
-    for(int r; r<6; r++){
-       bytes[r] = Wire.read();
-    }
-    Serial.print(bytes);
+    Serial.println(wb.int32);
     //call to function to decode message here and set registers as needed ect.
-}
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-void WireExtenderS::parse(){
-    // Parse the data
-    // 
 }
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -117,7 +117,9 @@ void WireExtenderS::register_write(){
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 void WireExtenderS::register_select(){
-      
+  // set the register to be read 
+  // set type_select to selected register type
+  // if write buff has data write it to selected buffer
 }
 
 
